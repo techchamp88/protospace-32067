a38931812c3b7010994e70b5621307beb844f0d8
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-11-26 16:29:31 +0900

    投稿機能の実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,8 +2,8 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title,            presence: true
-  validates :catch_copy,       presence: true
-  validates :concept,          presence: true
-  validates :image,            presence: true
+  validates :title,        presence: true
+  validates :catch_copy,   presence: true
+  validates :concept,      presence: true
+  validates :image,        presence: true
 end
