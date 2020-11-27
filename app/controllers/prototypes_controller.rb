f1568895bb97e26e6031281d03e0a1156e77d7e9
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-11-27 15:23:48 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -37,6 +37,11 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
 
 
 
