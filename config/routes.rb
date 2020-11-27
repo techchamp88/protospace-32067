a16d30e6f636a2d6232ef6818688f995221002bd
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-11-27 21:43:53 +0900

    usersコントローラーの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :prototype do
     resources :comments, only: :create
+  resources :users, only: :show
   end
 end
