bcff622d1fb08c10029c44344e9be0bd61d47f33
techchamp88 <74140404+techchamp88@users.noreply.github.com>
2020-11-28 16:07:55 +0900

    未ログイン状態の人への制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
 
 
 
